e0091072e9107a2f3567bed46730a3d7f83f9c8b
ajilan <ajilann123@gmail.com>
2019-02-09 23:42:05 -0800

    the default directive is set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { basicHighlightDirective } from './basic-highlight/basic-highligh.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    basicHighlightDirective
   ],
   imports: [
     BrowserModule
